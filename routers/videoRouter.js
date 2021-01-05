9d9422177845f433b5c66dbfe28b9f61d2ecf694
jjonyo <jonghyuk96@cau.ac.kr>
2021-01-06 01:42:40 +0900

    video search, video delete, video update, etc

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -6,16 +6,24 @@ import {
     getUpload,
     postUpload,
     videoDetail,
-    editVideo,
+    postEditVideo,
+    getEditVideo,
     deleteVideo
   } from "../controllers/videoController";
 const videoRouter=express.Router();
 
+//Upload
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
 
+//Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
-videoRouter.get(routes.deleteVideo, deleteVideo);
+
+//Edit Video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+//Delete Video
+videoRouter.get(routes.deleteVideo(), deleteVideo);
 
 export default videoRouter;
