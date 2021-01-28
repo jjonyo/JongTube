825b75055dc92d61fbb881ed00f1812c4c276a4d
jjonyo <jonghyuk96@cau.ac.kr>
2021-01-28 22:58:47 +0900

    session, middleware(public,private)

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,26 @@
 import userRouter from "../routers/userRouter";
 import routes from "../routes"
+import User from "../models/User";
+import passport from "passport";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
-export const postJoin = (req,res) =>{
-  const {body : {name,email,password1,password2}}=req;
-  if (password1 !== password2){
+export const postJoin = async (req,res,next) =>{
+  const {body : {name,email,password,password2}}=req;
+  if (password !== password2){
     res.status(400);
     res.render("join",{pagetitle:"Join"});
   }else{
-    res.redirect(routes.home);
+    try {
+      const user = await User({
+        name,
+        email
+      });
+      await User.register(user, password);
+      next();
+    } catch (error) {
+      console.log(error);
+      res.redirect(routes.home);
+    }
   }
 };
 
@@ -16,9 +28,11 @@ export const getLogin = (req, res) => {
   res.render("login", { pageTitle: "Log In" });
 }
 
-export const postLogin = (req,res) => {
-  res.redirect(routes.home);
-}
+export const postLogin = passport.authenticate("local", {
+  failureRedirect: routes.login,
+  successRedirect: routes.home
+});
+
 export const logout = (req, res) => {
   // To Do : Process log out
   res.redirect(routes.home);
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -2,18 +2,19 @@ import express from"express";
 import {getJoin,postJoin, getLogin,postLogin, logout} from "../controllers/userController";
 import {home,search} from "../controllers/videoController";
 import routes from "../routes";
+import {onlyPublic} from "../middleware";
 
 const globalRouter=express.Router();
 
 globalRouter.get(routes.home,home);
 
-globalRouter.get(routes.join,getJoin); 
-globalRouter.post(routes.join,postJoin);
+globalRouter.get(routes.join,onlyPublic,getJoin); 
+globalRouter.post(routes.join,onlyPublic,postJoin,postLogin);
 
-globalRouter.get(routes.login,getLogin);
-globalRouter.post(routes.login,postLogin);
+globalRouter.get(routes.login,onlyPublic,getLogin);
+globalRouter.post(routes.login,onlyPublic,postLogin);
 
-globalRouter.get(routes.logout,logout);
+globalRouter.get(routes.logout,onlyPublic,logout);
 globalRouter.get(routes.search,search);
 
 export default globalRouter;
